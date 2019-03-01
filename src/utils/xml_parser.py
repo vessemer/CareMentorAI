e924eacd66bd1dae604647f357c9e18a9b78d7c8
vessemer <vessemer@gmail.com>
2019-03-01 21:30:21 +0100

    Parse names

--- src/utils/xml_parser.py
+++ src/utils/xml_parser.py
@@ -7,6 +7,11 @@ def parse_occlusion(obj):
     return occluded.text
 
 
+def parse_name(obj):
+    name = obj.find('{http://www.w3.org/1999/xhtml}name')
+    return name.text
+
+
 def parse_polygon(obj):
     polygon = obj.find('{http://www.w3.org/1999/xhtml}polygon')
     points = polygon.findall('{http://www.w3.org/1999/xhtml}pt')
@@ -15,9 +20,9 @@ def parse_polygon(obj):
     for point in points:
         x = point.find('{http://www.w3.org/1999/xhtml}x')
         y = point.find('{http://www.w3.org/1999/xhtml}y')
-        x, y = int(x.text), int(y.text)
-        coords.append((x, y))
-    return coords
+        x, y = x.text, y.text
+        coords.extend((x, y))
+    return ' '.join(coords)
 
 
 def compose_dataframe(data, row):
@@ -37,9 +42,11 @@ def parse_xml(row):
     for obj in objects:
         occlusion = parse_occlusion(obj)
         coords = parse_polygon(obj)
+        name = parse_name(obj)
 
         data.append({
             'occluded': occlusion, 
             'coords': coords, 
+            'name': name,
         })
     return compose_dataframe(data, row)
